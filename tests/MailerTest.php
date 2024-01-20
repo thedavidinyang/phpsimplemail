84df07914bc160e8687f4d24ed5ca05747934c79
David Inyang <davidinyang01@gmail.com>
2024-01-20 02:57:26 +0100

    testing

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -4,7 +4,33 @@ namespace thedavidinyang\SimpleMail;
 
 use PHPUnit\Framework\TestCase;
 
+
 final class MailerTest extends TestCase
 {
+
+  
+
+    public function testClassConstructor()
+{
+    $e = new Mailer;
+
+    $this->assertNotEmpty($e->mail);
+
+
+} 
+
+
+
+public function testinit()
+{
+    $e = new Mailer;
+
+   $setup =  $e->init(['host' => '', 'username'=>'', 'password'=>'', 'authentication'=>'', 'port'=>''  ]);
+
+
+
+    $this->assertNotEmpty($setup);
+
+}
    
 }
