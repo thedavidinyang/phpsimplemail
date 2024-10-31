5fbac8dd81bc4a8800ec1790a3f1e082aff6c232
David Inyang <davidinyang01@gmail.com>
2024-10-31 13:03:42 +0100

    update

--- src/Mailer.php
+++ src/Mailer.php
@@ -11,26 +11,35 @@ namespace thedavidinyang\SimpleMail;
 
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPMailerSendGrid;
 use PHPMailer\PHPMailer\SMTP;
 
-use PHPMailer\PHPMailer\PHPMailerSendGrid;
-
-class Mailer 
+class Mailer
 {
     public $mail;
     private $settings;
 
     const VERSION = '1.5';
 
-
-    public function __construct()
+    public function __construct($type)
     {
 
-        $this->mail = new PHPMailer(true);
+        switch ($type) {
+
+            case 'SMTP':
+                $this->mail = new PHPMailer(true);
+
+                break;
+            case 'sendgrid':
+
+                $this->mail = new PHPMailerSendGrid(true);
+
+                break;
+        }
 
     }
 
-    private function smtp_setup(array $v) :array
+    private function smtp_setup(array $v): array
     {
         // $setup = [];
         $setup = $v;
@@ -47,10 +56,7 @@ class Mailer
         authentication = SSL or TLS
         port = smtp port
 
-
-
-
-        */
+         */
 
         // foreach ($v as $row) {
         //     $option = $row['option'];
@@ -67,67 +73,62 @@ class Mailer
     public function init($setup)
     {
 
-
         //Server settings
         $this->settings = $this->smtp_setup($setup);
-
 
         $this->mail->isSMTP(); //Send using SMTP
         $this->mail->Host = $this->settings['host']; //Set the SMTP server to send through
         $this->mail->SMTPAuth = true; //Enable SMTP authentication
-        $this->mail->Username = $this->settings['username']; 
-        $this->mail->Password = $this->settings['password']; 
+        $this->mail->Username = $this->settings['username'];
+        $this->mail->Password = $this->settings['password'];
         $this->mail->SMTPSecure = $this->settings['authentication'];
         // $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
         $this->mail->Port = $this->settings['port']; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-      
 
         return $this;
 
     }
 
+    /*
+    set mail format to
 
-    /* 
-    set mail format to 
-    
     true = html
     false = plain
 
-    */
-    
-    public function format(bool $v){
+     */
 
-        if($v){
+    public function format(bool $v)
+    {
+
+        if ($v) {
 
             $this->mail->isHTML(true); //Set email format to HTML
 
         } else {
             $this->mail->isHTML(false); //Set email format to HTML
 
-
         }
 
         return $this;
 
     }
 
-    // enable or diable debuging 
+    // enable or diable debuging
 
-    public function debug(bool $v){
+    public function debug(bool $v)
+    {
 
-        if($v){
+        if ($v) {
             //Enable verbose debug output
-        $this->mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+            $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-        } else{
+        } else {
             //Disable verbose debug output
-            $this->mail->SMTPDebug = SMTP::DEBUG_OFF ;  //Enable verbose debug output
+            $this->mail->SMTPDebug = SMTP::DEBUG_OFF; //Enable verbose debug output
 
         }
 
         return $this;
-
 
     }
 
@@ -140,7 +141,6 @@ class Mailer
         return $this;
 
     }
-
 
     /// set sender
     public function from(array $v)
@@ -155,8 +155,6 @@ class Mailer
 
         return $this;
     }
-
-
 
     /// set recipient
     public function to(array $v)
@@ -175,19 +173,16 @@ class Mailer
 
     }
 
-
     /// set many recipient
     public function to_many(array $v)
     {
-
-      
 
         // $v = [
         //     'email' => 'davidinyang01@gmail.com',
         //     'email'  => 'davidinyang01@gmail.com'
         // ];
 
-        foreach ($v as $e){
+        foreach ($v as $e) {
 
             $this->mail->addAddress($v['email']);
 
@@ -196,7 +191,6 @@ class Mailer
         return $this;
 
     }
-
 
     /// set recipient
     public function reply_to(array $v)
@@ -215,14 +209,15 @@ class Mailer
 
     // add bcc
 
-    public function bcc(array $v){
+    public function bcc(array $v)
+    {
 
-          // $v = [
+        // $v = [
         //     'email' => 'davidinyang01@gmail.com',
         //     'email'  => 'davidinyang01@gmail.com'
         // ];
 
-        foreach ($v as $e){
+        foreach ($v as $e) {
 
             $this->mail->addBCC($v['email']);
 
@@ -232,17 +227,17 @@ class Mailer
 
     }
 
-
     // add cc
 
-    public function cc(array $v){
+    public function cc(array $v)
+    {
 
-          // $v = [
+        // $v = [
         //     'email' => 'davidinyang01@gmail.com',
         //     'email'  => 'davidinyang01@gmail.com'
         // ];
 
-        foreach ($v as $e){
+        foreach ($v as $e) {
 
             $this->mail->addCC($v['email']);
 
@@ -251,7 +246,6 @@ class Mailer
         return $this;
 
     }
-
 
     // set subject
     public function subject(string $v)
@@ -263,7 +257,6 @@ class Mailer
 
     }
 
-
 // set   mail content
     public function body(string $v)
     {
@@ -271,40 +264,37 @@ class Mailer
         //Content
         $this->mail->Body = $v;
 
-
         return $this;
 
     }
-
 
     //set alternate content This is the body in plain text for non-HTML mail clients
 
-    public function alt_body(string $v){
+    public function alt_body(string $v)
+    {
 
-         //Content
-         $this->mail->AltBody = $v;
+        //Content
+        $this->mail->AltBody = $v;
 
         return $this;
 
     }
-
 
     /// send email
 
     public function sendmail(bool $response = false)
     {
 
-
         try {
 
             $this->mail->send();
 
-            if ($response){
+            if ($response) {
                 return true;
             }
         } catch (Exception $e) {
 
-            if ($response){
+            if ($response) {
                 return ("Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}");
             }
         }
